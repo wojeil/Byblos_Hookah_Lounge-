327f03db020b402bcd246433a26e708258cd28fd
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-06-16 10:32:39 -0400

    setting up Jumbo comp and nav has been set up

---
+++ client/src/Jumbotron/Jumbotron.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Jumbotron() {
+
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Fluid jumbotron</h1>
+                <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            </div>
+        </div>
+    );
+
+}
+
+
+export default Jumbotron;
---
+++ client/src/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+function Navbar() {
+
+    return (
+        <nav className="navbar bg-dark navbar-expand-lg navbar-dark">
+            <a className="navbar-brand" href="">Byblos Hookah Lounge</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="">Events</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="">Menue</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
