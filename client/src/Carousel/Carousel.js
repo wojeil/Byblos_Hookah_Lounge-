0768d22a9e2766607b4b7259ed3fc547cc193131
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-06-26 11:05:23 -0400

    everything ref carousel in place,, time to CSS it

--- client/src/Carousel/Carousel.js
+++ client/src/Carousel/Carousel.js
@@ -11,23 +11,15 @@ function Carousel() {
             <div className="carouselitem-inner">
 
                 <div className="carousel-item active">
-                    <div className="card">
-                        <div className="card-body">
-                            <img  className="event-image" src="https://blogmedia.evbstatic.com/wp-content/uploads/wpmulti/sites/8/shutterstock_199419065.jpg" alt="party pic" />
-                            <p id="test">first</p>
-                        </div>
-                    </div>
+                    <img className="event-image" src="https://blogmedia.evbstatic.com/wp-content/uploads/wpmulti/sites/8/shutterstock_199419065.jpg" alt="party pic" />
+                    <p id="test">first</p>
                 </div>
 
                 <div className="carousel-item">
-                    <div className="card">
-                        <div className="card-body">
-                            <img  className="event-image" src="" alt="party pic" />
-                            <p id="test">second page</p>
-                        </div>
-                    </div>
+                    <h2 className="testimonial-text">ttttttttttttttttttttttttttt</h2>
+                    <img className="testimonial-image" src="images/lady-img.jpg" alt="lady-profile" />
+                    <em></em>
                 </div>
-
                 <a className="carousel-control-prev" href="#event-carousel" role="button" data-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span className="sr-only">Previous</span>
