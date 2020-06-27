e713f8dfb5fe5d55b4785c611e4c5f934cdcf282
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-06-27 01:59:25 -0400

    added background image and link to fb

--- client/src/Navbar/Navbar.js
+++ client/src/Navbar/Navbar.js
@@ -5,16 +5,13 @@ import "./Navbar.css";
 function Navbar() {
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark sticky-top">
-            <a className="navbar-brand" href="">Byblos Hookah Lounge</a>
+        <nav className="navbar navbar-expand-lg navbar-dark">
+            <a className="navbar-brand"  target="_blank" href="https://www.facebook.com/bybloshookah/">Byblos Hookah Lounge</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <a className="nav-link" href="#about">About</a>
-                    </li>
                     <li className="nav-item">
                         <a className="nav-link" href="#events">Events</a>
                     </li>
