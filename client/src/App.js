4392813c9a61d71faff66202a3d5fe61a9931b64
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-06-29 11:56:15 -0400

    needs feature updates

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Jumbotron from "./Jumbotron/Jumbotron";
 import Carousel from "./Carousel/Carousel";
 import CarouselMenu from "./CarouselMenu/CarouselMenu";
 import ContactCard from "./ContactCard/ContactCard";
+import Footer from "./Footer/Footer";
 
 
 
@@ -53,18 +54,21 @@ function App() {
       {/* contact section */}
       <section id="contact">
         <div className="row">
-          <div className="col-sm-5">
+          <div className="col-sm-6">
             <div className="wrapper">
             <ContactCard/>
             </div>
             
           </div>
-          <div className="col-sm-3">
+          <div className="col-sm-2">
          
           </div>
           <div id="smoke-pic"className="col-sm-4"> <iframe  className="iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.15722450949!2d-81.20954528491761!3d28.62504958242065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e7690ff1fde2a1%3A0x10305d8eddf8afdc!2sByblos%20Hookah%20Lounge!5e0!3m2!1sen!2sus!4v1593407190972!5m2!1sen!2sus"  aria-hidden="false"/></div>
         </div>
+        <Footer/>
       </section>
+
+  
     
     </div>
   );
---
+++ client/src/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+
+function Footer(){
+
+    return(
+        
+          <footer className="container-fluid" >
+            <p className="footer-text"> ©Copyright <a className="foots"href="https://github.com/wojeil" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-square"></i></a> <a className="foots" href="https://www.linkedin.com/in/wadih-ojeil/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a></p>
+            </footer>
+        
+    )
+}
+
+export default Footer;
